b1bebe8552859f111ee0cf6d9cac4eff0a70924b
Neto Concon <concon.neto2@gmail.com>
2018-10-29 14:07:54 -0300

    logic working

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,13 +2,12 @@ class QuestionsController < ApplicationController
   def answer
     @ask = params[:ask]
 
-  if @ask.end_with?('?')
-    @answer = 'Silly question go to work'
-  elsif @ask == "I am going to work"
-    @answer = "Great!"
-  else
-    @answer = 'sei la'
-
+    if @ask.end_with?('?')
+      @answer = 'Silly question go to work'
+    elsif @ask == "I am going to work"
+      @answer = "Great!"
+    else
+      @answer = 'I don\'t care, get dressed and go to work!'
     end
   end
 
